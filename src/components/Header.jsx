870d9fa7f3135d7406e251726d4e20bf69b537ac
velTrinitas <71570164+velTrinitas@users.noreply.github.com>
2021-03-30 00:41:05 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Mariusz's Keeper</h1>
+      <p>Aplikacja nie jest moim autorskim projektem. Została z budowana w oparciu o kurs Udemy (Web Development Bootcamp)</p>
+    </header>
+  );
+}
+
+export default Header;
